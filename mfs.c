9d53e8fadfee28a6ab07b07f25e28db9ffabe6de
nicolen21 <93563036+nicolen21@users.noreply.github.com>
2022-04-11 18:14:35 -0500

    Add files via upload

---
+++ mfs.c
@@ -0,0 +1,171 @@
+// The MIT License (MIT)
+// 
+// Copyright (c) 2020 Trevor Bakker 
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+
+#define MAX_NUM_ARGUMENTS 3
+
+#define WHITESPACE " \t\n"      // We want to split our command line up into tokens
+                                // so we need to define what delimits our tokens.
+                                // In this case  white space
+                                // will separate the tokens on our command line
+
+#define MAX_COMMAND_SIZE 255    // The maximum command-line size
+
+// FAT32 variables
+char BS_OEMName[8];
+int16_t BPB_BytesPerSec; 
+int8_t BPB_SecPerClus;
+int16_t BPB_RsvdSecCnt;
+int8_t BPB_NumFATs;
+int16_t BPB_RootEntCnt;
+char BS_VolLab[11];
+int32_t BPB_FATSz32;
+int32_t BPB_RootClus; 
+
+// given in class
+struct DirectoryEntry 
+{
+  char DIR_Name[11];
+  uint8_t DIR_Attr;
+  uint8_t Unused1[8];
+  uint16_t DIR_FirstClusterHigh;
+  uint8_t Unused2[4];
+  uint16_t DIR_FirstClusterLow;
+  uint32_t DIR_FileSize;
+};
+struct DirectoryEntry dir[16];
+
+//opening file
+void openFile(char* filename) 
+{
+  // opening file
+  f = fopen(filename, 'r');
+
+  if(f != NULL)
+  {
+    //if file is open, get all BPB variables and then calculate the 
+    //root dir location
+  }
+  else if(//file is already open)
+  {
+    printf("");
+  }
+  else
+  {
+    printf("Error: File system image not found\n");
+  }
+}
+
+void closeFile(char* filename) 
+{
+  //if open (1), fclose file, set open var to 0
+}
+
+/*This command shall print out 
+information about the file system in 
+both hexadecimal and base 10
+*/
+void fileInfo() 
+{
+  //if open, print info
+  //else print that no file is open
+}
+
+/*This command shall print the attributes and 
+  starting cluster number of the file or directory name.  
+  If the parameter is a directory name then the size shall be 0. 
+  If the file or directory does not exist 
+  then your program shall output “Error: File not found”.
+*/
+void fileStat()
+{
+
+}
+
+int main()
+{
+
+  char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
+
+  while( 1 )
+  {
+    // Print out the mfs prompt
+    printf ("mfs> ");
+
+    // Read the command from the commandline.  The
+    // maximum command that will be read is MAX_COMMAND_SIZE
+    // This while command will wait here until the user
+    // inputs something since fgets returns NULL when there
+    // is no input
+    while( !fgets (cmd_str, MAX_COMMAND_SIZE, stdin) );
+
+    /* Parse input */
+    char *token[MAX_NUM_ARGUMENTS];
+
+    int   token_count = 0;                                 
+                                                           
+    // Pointer to point to the token
+    // parsed by strsep
+    char *arg_ptr;                                         
+                                                           
+    char *working_str  = strdup( cmd_str );                
+
+    // we are going to move the working_str pointer so
+    // keep track of its original value so we can deallocate
+    // the correct amount at the end
+    char *working_root = working_str;
+
+    // Tokenize the input stringswith whitespace used as the delimiter
+    while ( ( (arg_ptr = strsep(&working_str, WHITESPACE ) ) != NULL) && 
+              (token_count<MAX_NUM_ARGUMENTS))
+    {
+      token[token_count] = strndup( arg_ptr, MAX_COMMAND_SIZE );
+      if( strlen( token[token_count] ) == 0 )
+      {
+        token[token_count] = NULL;
+      }
+        token_count++;
+    }
+
+    // Now print the tokenized input as a debug check
+    // \TODO Remove this code and replace with your FAT32 functionality
+
+    int token_index  = 0;
+    for( token_index = 0; token_index < token_count; token_index ++ ) 
+    {
+      printf("token[%d] = %s\n", token_index, token[token_index] );  
+    }
+
+    free( working_root );
+
+  }
+  return 0;
+}
