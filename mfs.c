4465e6ab9e4ae5fd98316259f46f6c9da3d07a3c
bridgetg19 <60526814+bridgetg19@users.noreply.github.com>
2022-04-25 22:19:23 -0500

    Add files via upload
    CORRECT VERSIONNNNNN

--- mfs.c
+++ mfs.c
@@ -24,7 +24,6 @@
 #define _GNU_SOURCE
 
 #include <stdio.h>
-#include <stdint.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
@@ -32,8 +31,9 @@
 #include <string.h>
 #include <signal.h>
 #include <ctype.h>
+#include <stdint.h>
 
-#define MAX_NUM_ARGUMENTS 3
+#define MAX_NUM_ARGUMENTS 4
 
 #define WHITESPACE " \t\n"      // We want to split our command line up into tokens
 // so we need to define what delimits our tokens.
@@ -53,7 +53,7 @@ int16_t BPB_RootEntCnt;
 char BS_VolLab[11];
 int32_t BPB_FATSz32;
 int32_t BPB_RootClus;
-int8_t BPB_NumFATS;
+//int8_t BPB_NumFATS;
 
 // given in class
 struct DirectoryEntry
@@ -83,7 +83,7 @@ char name[12]; //reserve 12 characters for file name (11 bits plus 1 null)
 // - converts foo.txt to FOO     TXT
 int compareFilename(char *fn)
 {
-  // reserve 12 bits for filename (11 for filename + 1 for null terminator)
+   // reserve 12 bits for filename (11 for filename + 1 for null terminator)
    char expanded_name[12];
    memset(expanded_name, ' ', 12);
 
@@ -117,78 +117,78 @@ int compareFilename(char *fn)
       }
    }
 
-  return -1;
+   return -1;
 }
+
 
 //COMPARE FOR CD
 int compare(char *_input, char *_DIR_Name)
 {
-  char IMG_Name[11];
+   char IMG_Name[11];
 
-  char input[11];
-  char expanded_name[12];
+   char input[11];
+   char expanded_name[12];
 
-  strncpy(IMG_Name, _DIR_Name, 11);
-  strncpy(input, _input, 11);
+   strncpy(IMG_Name, _DIR_Name, 11);
+   strncpy(input, _input, 11);
 
-  if(strncmp(_input, "..", 2) != 0)
-  {
-    memset(expanded_name, ' ', 12);
-    char *token = strtok(input, ".");
-    strncpy(expanded_name, token, strlen(token));
-    token = strtok(NULL, ".");
+   if(strncmp(_input, "..", 2) != 0)
+   {
+      memset(expanded_name, ' ', 12);
+      char *token = strtok(input, ".");
+      strncpy(expanded_name, token, strlen(token));
+      token = strtok(NULL, ".");
 
-    if(token)
-    {
-      strncpy((char *)(expanded_name + 8), token, strlen(token));
-    }
+      if(token)
+      {
+         strncpy((char *)(expanded_name + 8), token, strlen(token));
+      }
 
-    expanded_name[11] = '\0';
+      expanded_name[11] = '\0';
 
-    int i;
-    for (i = 0; i < 11; i++)
-    {
-      expanded_name[i] = toupper(expanded_name[i]);
-    }
-  }
-  else
-  {
-    strncpy(expanded_name, "..", 2);
-    expanded_name[3] = '\0';
-    if(strncmp(expanded_name, IMG_Name, 2) == 0)
-    {
+      int i;
+      for (i = 0; i < 11; i++)
+      {
+         expanded_name[i] = toupper(expanded_name[i]);
+      }
+   }
+   else
+   {
+      strncpy(expanded_name, "..", 2);
+      expanded_name[3] = '\0';
+      if(strncmp(expanded_name, IMG_Name, 2) == 0)
+      {
+         return 0;
+      }
+      return 1;
+   }
+
+   if(strncmp(expanded_name, IMG_Name, 11) == 0)
+   {
       return 0;
-    }
-    return 1;
-  }
-
-  if(strncmp(expanded_name, IMG_Name, 11) == 0)
-  {
-    return 0;
-  }
-  return 1;
+   }
+   return 1;
 }
 
-
 /* (From FAT-1.pdf on Canvas)
-   Function: LBAtoOffset
-   Parameters: The current sector number that points to a block of data
-   Returns: The value of the address for that block of data
-   Description: Finds the starting address of a block of data given the sector number
-                corresponding to that data block
+Function: LBAtoOffset
+Parameters: The current sector number that points to a block of data
+Returns: The value of the address for that block of data
+Description: Finds the starting address of a block of data given the sector number
+corresponding to that data block
 */
 int LBAtoOffset(int32_t sector)
 {
    return ((sector - 2) * BPB_BytesPerSec) + (BPB_BytesPerSec * BPB_RsvdSecCnt)
-            + (BPB_NumFATS * BPB_FATSz32 * BPB_BytesPerSec);
+   + (BPB_NumFATs * BPB_FATSz32 * BPB_BytesPerSec);
 }
 
 
 /* (from FAT-1.pdf on Canvas)
-   Name: NextLB
-   Purpose: Given a logical block address, look up into the first FAT and return the
-            logical block address of the block in the file. If there is no further blocks
-            then return -1
+Name: NextLB
+Purpose: Given a logical block address, look up into the first FAT and return the
+logical block address of the block in the file. If there is no further blocks
+then return -1
 */
 int16_t NextLB(uint32_t sector)
 {
@@ -244,7 +244,7 @@ void openFile(char* fn) //5 points
 
          // file pointer placed at root
          fseek(fp, BPB_NumFATs * (BPB_FATSz32 * BPB_BytesPerSec) +
-                                 (BPB_RsvdSecCnt * BPB_BytesPerSec), SEEK_SET);
+         (BPB_RsvdSecCnt * BPB_BytesPerSec), SEEK_SET);
          fread(&dir[0],sizeof(struct DirectoryEntry), 16, fp);
       }
       else
@@ -275,6 +275,7 @@ void closeFile() //5 points
       printf("Error: File system not open.\n");
    }
 }
+//still need to add "Error: File system image must be opened first" part
 
 
 /*
@@ -310,7 +311,7 @@ void fileInfo() //10 points
       //get BPB_NumFATS which starts at 16, size of 1
       // fseek(fp, 16, SEEK_SET);
       // fread(&BPB_NumFATS, 1, 1, fp);
-      printf("BPB_NumFATS: \t\t%d \t%x\n", BPB_NumFATS, BPB_NumFATS);
+      printf("BPB_NumFATS: \t\t%d \t%x\n", BPB_NumFATs, BPB_NumFATs);
 
       //get BPB_FATSz32 which starts at 36, size of 4
       // fseek(fp, 36, SEEK_SET);
@@ -398,7 +399,7 @@ void getFile(char *fn) //15 points
       }
 
       fseek(fp, 16, SEEK_SET);
-      fread(&BPB_NumFATS, 1, 1, fp);
+      fread(&BPB_NumFATs, 1, 1, fp);
 
       // - get low cluster number
       int cluster = dir[index].DIR_FirstClusterLow;
@@ -454,68 +455,70 @@ void getFile(char *fn) //15 points
 /*
 This command shall change the current working directory
 to the given directory. Your program shall support relative
-paths, e.g cd ../name and absolute paths. (ABSOLUTE PATH IS EXTRA CREDIT)
+paths, e.g cd ../name and absolute paths. (HE SAID ABSOLUTE PATH
+DOESN'T NEED TO BE DONE IN CLASS)
 */
 void changeDir(char *fn) //10 points
 {
-  if(fileOpen)
-  {
-    for(i = 0; i < 16; i++)
-    {
-      if(!compare(fn, dir[i].DIR_Name))
+   if(fileOpen)
+   {
+      int i;
+      for(i = 0; i < 16; i++)
       {
-        int cluster = dir[i].DIR_FirstClusterLow;
+         if(!compare(fn, dir[i].DIR_Name))
+         {
+            int cluster = dir[i].DIR_FirstClusterLow;
 
-        if(cluster == 0)
-        {
-          cluster = 2;
-        }
-        int offset = LBAtoOffset(cluster);
+            if(cluster == 0)
+            {
+               cluster = 2;
+            }
+            int offset = LBAtoOffset(cluster);
 
-        fseek(fp, offset, SEEK_SET);
-        fread(&dir[0], sizeof(struct DirectoryEntry), 16, fp);
+            fseek(fp, offset, SEEK_SET);
+            fread(&dir[0], sizeof(struct DirectoryEntry), 16, fp);
 
-        break;
+            break;
+         }
       }
-    }
 
-    char *directoy;
-    directoy = strtok(fn, "/");
-    for(i = 0; i < 16; i++)
-    {
-      if(!compare(fn, dir[i].DIR_Name))
+      char *directoy;
+      directoy = strtok(fn, "/");
+      for(i = 0; i < 16; i++)
       {
-        int cluster = dir[i].DIR_FirstClusterLow;
-        int offset = LBAtoOffset(cluster);
+         if(!compare(fn, dir[i].DIR_Name))
+         {
+            int cluster = dir[i].DIR_FirstClusterLow;
+            int offset = LBAtoOffset(cluster);
 
-        if(cluster == 0)
-        {
-          cluster = 2;
-        }
+            if(cluster == 0)
+            {
+               cluster = 2;
+            }
 
-        fseek(fp, offset, SEEK_SET);
-        fread(&dir[0], sizeof(struct DirectoryEntry), 16, fp);
+            fseek(fp, offset, SEEK_SET);
+            fread(&dir[0], sizeof(struct DirectoryEntry), 16, fp);
 
-        break;
+            break;
+         }
       }
-    }
 
-    while((directoy = strtok(NULL, "/")))
-    {
-      int cluster = dir[i].DIR_FirstClusterHigh;
-      if(cluster == 0)
+      while((directoy = strtok(NULL, "/")))
       {
-        cluster = 2;
+         int cluster = dir[i].DIR_FirstClusterHigh;
+         if(cluster == 0)
+         {
+            cluster = 2;
+         }
+         int offset = LBAtoOffset(cluster);
+         fseek(fp, offset, SEEK_SET);
+         fread(&dir[0], sizeof(struct DirectoryEntry), 16, fp);
       }
-      int offset = LBAtoOffset(cluster);
-      fseek(fp, offset, SEEK_SET);
-      fread(&dir[0], sizeof(struct DirectoryEntry), 16, fp);
-    }
-  }
-  else
-  {
-    printf("Error: File system image must be opened first.\n");
-  }
+   }
+   else
+   {
+      printf("Error: File system image must be opened first.\n");
+   }
 }
 
 
@@ -542,7 +545,7 @@ void listDir() //10 points
          // - check if file is read only (0x01), subdirectory (0x10), or archive flag (0x20)
          // - check that its also not a deleted file (0xe5)
          if((dir[i].DIR_Attr == 0x01 || dir[i].DIR_Attr == 0x10 || dir[i].DIR_Attr == 0x20)
-            && dir[i].DIR_Name[0] != 0xe5)
+         && dir[i].DIR_Name[0] != '?')
          {
             //reserve 12 characters for file name (11 bits plus 1 null)
             char name[12];
@@ -564,12 +567,64 @@ Reads from the given file at the position, in bytes,
 specified by the position parameter and output
 the number of bytes specified.
 */
-void readFile() //10 points
+void readFile(char *fn, char *pos, char *n_b) //10 points
 {
-  if(fileOpen)
+   //if open, print info
+   //else print that no file is open
+   if(fileOpen)
    {
-      //code
+      //check if filename was given
+      if(fn == 0 || pos == NULL || n_b == NULL)
+      {
+         printf("Error: Missing parameter.\n");
+         return;
+      }
+
+      // - look up directory and compare filename for match
+      int index=compareFilename(fn);
+
+      //if no match was found
+      if(index==-1)
+      {
+         printf("Error: File not found.\n");
+         return;
+      }
+
+      // - reading in BPB variables
+      fseek(fp, 11, SEEK_SET);
+      fread(&BPB_BytesPerSec, 2, 1, fp);
+      fseek(fp, 13, SEEK_SET);
+      fread(&BPB_SecPerClus, 1, 1, fp);
+      fseek(fp, 14, SEEK_SET);
+      fread(&BPB_RsvdSecCnt, 2, 1, fp);
+      fseek(fp, 16, SEEK_SET);
+      fread(&BPB_NumFATs, 1, 1, fp);
+      fseek(fp, 36, SEEK_SET);
+      fread(&BPB_FATSz32, 4, 1, fp);
+
+      int position=atoi(pos);
+      int n_bytes=atoi(n_b);
+      int cluster = dir[index].DIR_FirstClusterLow;
+      // - calculate offset
+      int offset = LBAtoOffset(cluster);
+      // - fseek to that offset
+      fseek(fp, offset, SEEK_SET); //fp is pointing exactly where the file begins
+      // - create buffer with size 512 because 512 = BPB_BytesPerSec
+      uint8_t buffer[512];
+
+      // - fseek to the specified position depending on current position of fp
+      fseek(fp, position, SEEK_CUR);
+      // - read info into buffer for number of specified bytes
+      fread(buffer, n_bytes, 1, fp);
+
+      for(int i=0; i<n_bytes; i++)
+      {
+         printf("%d ", buffer[i]);
+      }
+      printf("\n");
+
    }
+
    else
    {
       printf("Error: File system image must be opened first.\n");
@@ -584,28 +639,28 @@ void deleteFile(char *fn) //10 points
 {
    if(fileOpen)
    {
-     int i = compareFilename(fn);
+      int i = compareFilename(fn);
 
-     if(i == -1)
-     {
-       printf("File not found.\n");
-       return;
-     }
-     else
-     {
-       //save attributes into a placeholder
-       attrHolder = dir[i].DIR_Attr;
-       //delete file by setting attribute to 0xe5
-       dir[i].DIR_Attr = 0xe5;
-       //save file name into a placeholder
-       strncpy(fnHolder, dir[i].DIR_Name, 11);
-       //change file name to show that it has been deleted
-       strcpy(dir[i].DIR_Name, "?");
-     }
+      if(i == -1)
+      {
+         printf("File not found.\n");
+         return;
+      }
+      else
+      {
+         //save attributes into a placeholder
+         attrHolder = dir[i].DIR_Attr;
+         //delete file by setting attribute to 0xe5
+         dir[i].DIR_Attr = 0xe5;
+         //save file name into a placeholder
+         strncpy(fnHolder, dir[i].DIR_Name, 11);
+         //change file name to show that it has been deleted
+         strcpy(dir[i].DIR_Name, "?");
+      }
    }
    else
    {
-     printf("Error: File system image must be opened first.\n");
+      printf("Error: File system image must be opened first.\n");
    }
 }
 
@@ -621,31 +676,31 @@ void restoreFile(char *fn) //10 points
 
    if(fileOpen)
    {
-     for(i = 0; i < 16; i++)
-     {
-       //if there is no deleted files, break out of loop
-       if(!strcmp(dir[i].DIR_Name,"?"))
-       {
-         deleted = 1;
-         break;
-       }
-     }
-     //if it is a deleted file
-     if(deleted)
-     {
-       //assign name placeholder to DIR_Name
-       strncpy(dir[i].DIR_Name, fnHolder, 11);
-       //assign attributes placeholder to DIR_Attr
-       dir[i].DIR_Attr = attrHolder;
-     }
-     else
-     {
-       printf("Error: File not found. \n");
-     }
+      for(i = 0; i < 16; i++)
+      {
+         //if there is no deleted files, break out of loop
+         if(!strcmp(dir[i].DIR_Name,"?"))
+         {
+            deleted = 1;
+            break;
+         }
+      }
+      //if it is a deleted file
+      if(deleted)
+      {
+         //assign name placeholder to DIR_Name
+         strncpy(dir[i].DIR_Name, fnHolder, 11);
+         //assign attributes placeholder to DIR_Attr
+         dir[i].DIR_Attr = attrHolder;
+      }
+      else
+      {
+         printf("Error: File not found. \n");
+      }
    }
    else
    {
-     printf("Error: File system image must be opened first.\n");
+      printf("Error: File system image must be opened first.\n");
    }
 }
 
@@ -745,7 +800,8 @@ int main()
 
       if(strcmp(token[0], "cd") == 0)
       {
-        changeDir(token[1]);
+         // pass in filename parameter (token[1])
+         changeDir(token[1]);
       }
 
       if(strcmp(token[0], "ls") == 0)
@@ -755,7 +811,8 @@ int main()
 
       if(strcmp(token[0], "read") == 0)
       {
-         //readFile(token[1], atoi(token[2]), atoi(token[3]));
+         // pass in filename parameter (token[1]), position (token[2]), number of bytes (token[3])
+         readFile(token[1], token[2], token[3]);
       }
 
       if(strcmp(token[0], "del") == 0)
